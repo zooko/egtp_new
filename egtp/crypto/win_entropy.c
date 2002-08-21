193d9bd3107f6f55fe0261c1a631a0d3af89c331
artimage <artimage>
2002-08-21 05:20:05 +0000

    Adding the MIT license to this file.

--- egtp/crypto/win_entropy.c
+++ egtp/crypto/win_entropy.c
@@ -6,7 +6,11 @@
  * the wincrypt module from MS. 
  *
  * artimage@ishiboo.com 2002
-*/
+ *
+ * Copyright (c) 2002 Artimage
+ *
+ * See end of file for License information.
+ */
 
 #include <Python.h>
 
@@ -82,3 +86,25 @@ int main(int argc, char **argv)
 
 
 #endif // WIN32
+
+/*
+ * License: 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software to deal in this software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of this software, and to permit
+ * persons to whom this software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of this software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THIS SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THIS SOFTWARE.
+ */
