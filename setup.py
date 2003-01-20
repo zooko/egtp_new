6dbbe90d4afdd236e3f6fa4fbae9e4e3f3721f14
myers_carpenter <myers_carpenter>
2003-01-20 03:30:41 +0000

    * adding to the build system

--- setup.py
+++ setup.py
@@ -1,100 +1,221 @@
 #!/usr/bin/env python
 
-import os, sys, string, re, urllib, md5, glob, pprint
+import os, sys, string, re, urllib, md5, glob, pprint, zipfile
 #import egtp.version
 
 try:
     from distutils.core import setup, Extension, Command
     import distutils.command.build_ext
-except:
-    raise SystemExit, """\ 
-Could not load the distutils modules. Have you
-installed them? On Debian (when you are using the version of python that
-came with Debian) you need to 'apt-get install python-dev'.  If you've installed 
-python from source you will need to figure this out yourself.
+except ImportError:
+    print """\
+You don't have the python development modules installed.  
+
+If you have Debian you can install it by running
+    apt-get install python-dev
+
+If you have RedHat and know how to install this from an RPM please
+email us so we can put instructions here.
 """
+    raise
+
+CVS_ANON_USER = ':pserver:anonymous'
 
 class download(Command):
     description  = "download any files needed to build"
+    
+    user_options = [
+        ('no-update', None,
+         "Don't do 'cvs update' in dependant modules"),
+        ('checkout-dir=', None,
+         "Directory to check out dependant modules into"),
+        ('username=', None,
+         "Username to use to CVS checkout/update"),]
 
-    user_options = []
-
+    boolean_options = ['no-update']
+    
     def initialize_options(self):
-        pass
-
+        self.no_update = None
+        self.username = None
+        self.checkout_dir = None
+                    
     def finalize_options(self):
-        pass
+        if not self.checkout_dir:
+            self.checkout_dir = 'build'
+        if not self.username:
+            self.username = CVS_ANON_USER
+        else:
+            self.username = ":ext:%s" % self.username
 
     def run(self):
-        if not os.path.isdir('build'):
-            os.mkdir('build')
-
         self.base_dir = os.getcwd()
-        if not os.path.isdir('build'):
-            os.mkdir('build')
-        self.download_bsddb3()
-        self.download_pyutil()
-        #self.download_libzutil()
-        #self.download_libzstr()
-        self.download_libbase32()
-        self.download_cryptopp()
+        if not os.path.isdir(self.checkout_dir):
+            os.mkdir(self.checkout_dir)
+        self.check_bsddb3()
+        self.check_pyutil()
+        self.check_libbase32()
+        self.check_cryptopp()
 
-    def download_bsddb3(self):
+    def check_bsddb3(self):
         try:
             import bsddb3
         except ImportError:
-            raise SystemExit, """\ 
-            
-The module bsddb3 (http://pybsddb.sourceforge.net/) must be installed.  (on
-Debian, when using the python that comes with Debian you can do 'apt-get
-install python-bsddb3'. For RedHat you can get an rpm from
-http://rpmfind.net/linux/RPM/sourceforge/pybsddb/bsddb3-3.3.0-1.i386.html 
+            print """\
+You don't have the python module 'bsddb3' installed.  
 
-If you've built python from source you will need to download the package
-from the first URL and build it yourself.
+You can find this module's homepage at http://pybsddb.sf.net/
+
+If you have Debian you can install it by running
+    apt-get install python-bsddb3
+
+If you have RedHat and know how to install this from an RPM please
+email us so we can put instructions here.
 """
+            raise
 
-    def download_pyutil(self):
-        try:
-            import pyutil
-            # TODO verison testing
-        except ImportError:
-            print "Downloading 'pyutil' ... "
-            my_build_dir = os.path.join('build', 'pyutil')
-            fetch_anon_cvs(':pserver:anonymous@cvs.pyutil.sourceforge.net:/cvsroot/pyutil', 'pyutil_new', my_build_dir)
-            os.chdir(my_build_dir)
-            distutils.core.run_setup('setup.py', ['install', '--install-lib', self.base_dir] )
-            os.chdir(self.base_dir)
+    def check_pyutil(self):
+        if self.no_update:
+            try:
+                import pyutil
+                return
+            except ImportError:
+                pass
+        my_build_dir = os.path.join(self.checkout_dir, 'pyutil')
+        fetch_anon_cvs(self.username, 'cvs.pyutil.sourceforge.net:/cvsroot/pyutil', 'pyutil_new', my_build_dir)
+        chdir_print(my_build_dir)
+        # check to see if this OS has symlinks
+        if hasattr(os, 'symlink'):
+            system_print_interrupt("%s setup.py build_ext --inplace" % sys.executable)
+            chdir_print(self.base_dir)
+            if os.path.islink('pyutil') and not self.no_update:
+                os.unlink('pyutil')
+            os.symlink(os.path.join(my_build_dir, 'pyutil'), 'pyutil')
+        else:
+            system_print_interrupt("%s setup.py install --lib-dir=%s" % (sys.executable, self.base_dir))
 
-    def download_libzutil(self):
-        if not os.path.isdir(os.path.join('build', 'libzutil')):
-            print "Downloading 'libzutil' ... "
-            my_build_dir = os.path.join('build', 'libzutil')
-            fetch_anon_cvs(':pserver:anonymous@cvs.libzutil.sourceforge.net:/cvsroot/libzutil', 'libzutil', my_build_dir)
-            os.chdir(my_build_dir)
-            os.system('make')
+    def check_libbase32(self):
+        if self.no_update:
+            try:
+                import base32
+                return
+            except ImportError:
+                pass                
+        my_build_dir = os.path.join(self.checkout_dir, 'libbase32')
+        fetch_anon_cvs(self.username, 'cvs.libbase32.sourceforge.net:/cvsroot/libbase32', 'libbase32', my_build_dir)
+        chdir_print(my_build_dir)
+        # check to see if this OS has symlinks
+        if hasattr(os, 'symlink'):
+            system_print_interrupt("%s setup.py build_ext --inplace" % sys.executable)
+            chdir_print(self.base_dir)
+            if os.path.islink('base32') and not self.no_update:
+                os.unlink('base32')
+            os.symlink(os.path.join(my_build_dir, 'base32'), 'base32')
+        else:
+            system_print_interrupt("%s setup.py install --lib-dir=%s" % (sys.executable, self.base_dir))
 
-    def download_libzstr(self):
-        if not os.path.isdir(os.path.join('build', 'libzstr')):
-            print "Downloading 'libzstr' ... "
-            my_build_dir = os.path.join('build', 'libzstr')
-            fetch_anon_cvs(':pserver:anonymous@cvs.libzstr.sourceforge.net:/cvsroot/libzstr', 'libzstr', my_build_dir)
-            os.chdir(my_build_dir)
-            os.system('make')
+    def check_cryptopp(self):
+        def _get_cryptopp():
+            CRYPTOPP_URL = 'http://www.eskimo.com/~weidai/crypto50.zip'
+            print "Downloading cryptopp from %s ... " % CRYPTOPP_URL
+            out = file(os.path.join('build', 'crypto50.zip'), 'wb')
+            out.write(urllib.urlopen(CRYPTOPP_URL).read())
+            out.close()
 
-    def download_libbase32(self):
-        try:
-            import base32
-            # TODO version testing
-        except ImportError:
-            print "Downloading 'libbase32' ... "
-            my_build_dir = os.path.join('build', 'libbase32')
-            fetch_anon_cvs(':pserver:anonymous@cvs.libbase32.sourceforge.net:/cvsroot/libbase32', 'libbase32', my_build_dir)
-            os.chdir(my_build_dir)
-            distutils.core.run_setup('setup.py', ['install', '--install-lib', self.base_dir] )
-            os.chdir(self.base_dir)
+        if not os.path.isfile(os.path.join(os.getcwd(), 'build', 'crypto50.zip')):
+            _get_cryptopp()
 
-    def download_cryptopp(self):
+        m = md5.new()
+        m.update(file(os.path.join('build', 'crypto50.zip'), 'rb').read())
+        if m.hexdigest().lower() != 'fe8d4ef49b69874763f6dab30cbb6292':
+            raise SystemExit, "File %r is incomplete or corrupt" % os.path.join('build', 'crypto42.zip')
+
+        if not os.path.isfile(os.path.join('build', 'crypto50.zip')):
+            raise SystemExit, "couldn't get zip file"
+
+        def _unzip(zipfilename, extractdir):
+            zf = zipfile.ZipFile(zipfilename, "r")
+            for afile in zf.infolist():
+                try: 
+                    os.makedirs(extractdir)
+                except: 
+                    pass
+                                        
+                if afile.file_size > 0:
+                    temp = zf.read(afile.filename)
+                    fname =  os.path.join(extractdir, zfile.filename)
+                    print fname
+                    ff = open(fname, "wb")
+                    ff.write(temp)
+                    ff.close()
+
+        if not os.path.isdir(os.path.join('build', 'crypto50')):
+            print "Extracting crypto++..."
+            cmd = 'unzip -d %s -a %s' % (os.path.join('build', 'crypto50'), os.path.join('build', 'crypto50.zip'))
+            print cmd
+            os.system(cmd)
+            # same effect as 'touch'
+            file(os.path.join('build', 'crypto50', 'unpatched'),'w').close()
+
+        if os.path.isfile(os.path.join('build', 'crypto50', 'unpatched')):
+            print "Patching crypto50..."
+            chdir_print(os.path.join(self.base_dir, 'build', 'crypto50'))
+            patchlist = os.listdir(os.path.join(self.base_dir, 'egtp', 'crypto', 'patches'))
+            patchlist = filter(lambda x: x[0].islower(), patchlist)
+            for ii in patchlist:
+                os.system('patch -p0 < %s' % os.path.join(self.base_dir, 'egtp', 'crypto', 'patches', ii))
+            chdir_print(self.base_dir)
+            os.unlink(os.path.join('build', 'crypto50', 'unpatched'))
+
+        if not os.path.isfile(os.path.join('build', 'crypto50', 'libcryptopp.a')):
+            print "Building crypto++..."
+            chdir_print(os.path.join(self.base_dir, 'build', 'crypto50'))
+            os.system('make libcryptopp.a')
+            chdir_print(self.base_dir)
+
+def chdir_print(dirname):
+    dirname = os.path.abspath(dirname)
+    print "[ now entering %r ... ]" % dirname
+    os.chdir(dirname)
+    
+def system_print_interrupt(cmd):
+    print cmd
+    ret = os.system(cmd)
+    # print "os.system returned %d" % os.WIFSIGNALED(ret)
+    # if os.WIFSIGNALED(ret) == 2:
+    #    raise KeyboardInterrupt
+
+def fetch_anon_cvs(username, serverpath, module, directory_name):
+    """
+    Fetchs or updates a module from CVS.  Write the module into the current directory.
+    """
+    cvsroot = "%s@%s" % (username, serverpath)
+    
+    cwd = os.getcwd()
+    
+    if username == CVS_ANON_USER:
+        write_cvs_pass = 1
+        if os.path.isfile(os.path.expandvars('${HOME}/.cvspass')):
+            ff = file(os.path.expandvars('${HOME}/.cvspass'), 'r')
+            tmp = ff.read()
+            ff.close()
+            tmp = re.findall(re.escape(cvsroot), tmp)
+            if tmp:
+                write_cvs_pass = 0
+        if write_cvs_pass:
+            ff = file(os.path.expandvars('${HOME}/.cvspass'), 'a')
+            ff.write("%s A\n" % cvsroot)
+    if os.path.isdir(directory_name):
+        print "Updating %r..." % module
+        os.chdir(directory_name)
+        system_print_interrupt('cvs -z3 update -Pd')
+    else:
+        print "Checking out %r..." % module
+        subdir, final_dir = os.path.split(os.path.abspath(directory_name))
+        os.chdir(subdir)
+        cmd = 'cvs -z3 -d%s checkout -d%s -P %s' % (cvsroot, final_dir, module)
+        system_print_interrupt(cmd)
+    os.chdir(cwd)
+
+    def check_cryptopp(self):
         def get_cryptopp():
             CRYPTOPP_URL = 'http://www.eskimo.com/~weidai/crypto50.zip'
             print "Downloading cryptopp from %s ... " % CRYPTOPP_URL
@@ -276,36 +397,6 @@ class test(Command):
 
         sys.path = old_path[:]
 
-def fetch_anon_cvs(cvsroot, module, directory_name):
-    """
-    Fetchs or updates a module from CVS.  Write the module into the current directory.
-    """
-    print "Downloading module '%s' from CVS ROOT '%s' into directory '%s' ..." % (module, cvsroot, directory_name)
-
-    cwd = os.getcwd()
-
-    write_cvs_pass = 1
-    if os.path.isfile(os.path.expandvars('${HOME}/.cvspass')):
-        ff = open(os.path.expandvars('${HOME}/.cvspass'), 'r')
-        tmp = ff.read()
-        ff.close()
-        tmp = re.findall(re.escape(cvsroot), tmp)
-        if tmp:
-            write_cvs_pass = 0
-    if write_cvs_pass:
-        ff = open(os.path.expandvars('${HOME}/.cvspass'), 'a')
-        ff.write("%s A\n" % cvsroot)
-    if os.path.isdir(directory_name):
-        print "Updating %r..." % module
-        os.chdir(directory_name)
-        os.system('cvs -z3 -d%s update -Pd' % cvsroot)
-    else:
-        print "Checking out %r..." % module
-        subdir, final_dir = os.path.split(os.path.abspath(directory_name))
-        os.chdir(subdir)
-        cmd = 'cvs -z3 -d%s checkout -d%s -P %s' % (cvsroot, final_dir, module)
-        os.system(cmd)
-    os.chdir(cwd)
 
 setup_args = {
     'name': "egtp",
